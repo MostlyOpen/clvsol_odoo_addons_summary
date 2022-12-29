1341cc8fc7b116076df39983a5155ae6b63696cc
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2022-12-29 14:42:07 -0300

    [IMP] clv_summary (IMP __manifest__.py)

--- clv_summary/__manifest__.py
+++ clv_summary/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': 'Summary',
     'summary': 'Summary Module used by CLVsol Solutions.',
-    'version': '14.0.5.0',
+    'version': '15.0.6.0',
     'author': 'Carlos Eduardo Vercelino - CLVsol',
     'category': 'CLVsol Solutions',
     'license': 'AGPL-3',
